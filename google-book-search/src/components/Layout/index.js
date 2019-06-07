c5bc46387582e7bbf43e7cdcec47b677895aa8c8
Giovanny Caquias <giovannycaquias.esq@gmail.com>
2019-06-07 15:59:34 -0400

    9th commit

--- google-book-search/src/components/Layout/index.js
+++ google-book-search/src/components/Layout/index.js
@@ -1,24 +1,22 @@
 import React from "react";
 
 export function Container({ fluid, children }) {
-  return <div className={`container${fluid ? "-fluid" : ""}`}>{children}</div>;
+    return <div className={`container${fluid ? "-fluid" : ""}`}>{children}</div>;
 }
 
-
-  return <div className={`row${fluid ? "-fluid" : ""}`}>{children}</div>;
+export function Row({ fluid, children }) {
+    return <div className={`row${fluid ? "-fluid" : ""}`}>{children}</div>;
 }
 
-// This Col component lets us size bootstrap columns with less syntax
-// e.g. <Col size="md-12"> instead of <div className="col-md-12">
 export function Col({ size, children }) {
-  return (
+    return (
     <div
-      className={size
+        className={size
         .split(" ")
         .map(size => "col-" + size)
         .join(" ")}
     >
-      {children}
+        {children}
     </div>
-  );
+    );
 }
