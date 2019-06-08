904a3e8d98a02c70dc772084c98cab0497de63c1
Giovanny Caquias <giovannycaquias.esq@gmail.com>
2019-06-08 11:02:59 -0400

    29th commit

--- google-book-search/controllers/bookController.js
+++ google-book-search/controllers/bookController.js
@@ -1,16 +1,16 @@
 const db = require("../models");
 
 module.exports = {
-    findAll: function(req, res) {
+    allResults: function(req, res) {
         db.Book
             .find(req.query)
             .sort({ date: -1 })
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
-    findById: function(req, res) {
+    idResults: function(req, res) {
         db.Book
-            .findById(req.params.id)
+            .idResults(req.params.id)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
@@ -28,7 +28,7 @@ module.exports = {
     },
     remove: function(req, res) {
         db.Book
-            .findById({ _id: req.params.id })
+            .idResults({ _id: req.params.id })
             .then(dbModel => dbModel.remove())
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
